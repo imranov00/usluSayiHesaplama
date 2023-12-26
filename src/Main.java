3ac549a7ebf2ddf354bdaa5a56d19009710d0657
İmranov <semtexpert@gmail.com>
2023-12-26 15:07:18 +0300

    versiyon 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n;
+        int k;
+        int total=1;
+
+
+        Scanner inp =new Scanner(System.in);
+        System.out.println("Üssü Alınacak Sayı:");
+        n= inp.nextInt();
+        System.out.println("Üs Olacak Sayı :");
+        k = inp.nextInt();
+
+
+        for (int i = 1; i<=k; i++){
+            total *=n;
+
+        }
+        System.out.println("Üslü Sayınız: "  + total);
+
+
+
+
     }
 }
